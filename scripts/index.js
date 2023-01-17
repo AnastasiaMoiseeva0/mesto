4e51fb3a731081eb35304996f3e91b6fdf5f3e0e
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-01-17 22:24:04 +0700

    fix: Исправление после первого ревью

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+/* "Элементы Найдены по id и сохранены в переменные */
+let editProfileButton = document.querySelector('#edit-profile-button');
+
+let closePopupButton = document.querySelector('#closeButton');
+
+let popup = document.querySelector('#popup');
+let formElement = document.querySelector('#formElement');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+
+/* Функция открытия popup*/
+
+function openEditProfilePopup() {
+  popup.classList.add('popup_opened');
+}
+
+/* Функция сохранения изменений, вносимых пользователем, закрытие модального окна при нажатии на кнопку 'сохранить'*/
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  let profileName = document.querySelector('#profileName');
+  let profileProfession = document.querySelector('#profileProfession');
+
+  profileName.textContent = name;
+  profileProfession.textContent = job;
+
+  closePopup();
+}
+
+/* Добавлена обработка событий с помощью addEventListener */
+
+formElement.addEventListener('submit', handleFormSubmit);
+closePopupButton.addEventListener('click', closePopup);
+
+/* Функция закрытия popup */
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+
+}
+
+editProfileButton.addEventListener('click', openEditProfilePopup);
