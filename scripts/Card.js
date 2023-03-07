558d693bc6062134a25a55974160cd9babe2a2b0
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-03-07 15:25:35 +0700

    fix: id изменены на классы

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,36 +1,42 @@
 export class Card {
-    constructor(cardData, placeTemplate, imagePopup) {
-      this._placeTemplate = placeTemplate;
-      this._cardData = cardData;
-      this._imagePopup = imagePopup;
-    }
-  
-    getElement() {
-        const newCard = this._placeTemplate.firstElementChild.cloneNode(true);
-        const newCardImage = newCard.querySelector("#urlPlace");
-        this._setEventListeners(newCard, newCardImage);
-        this._setAttributes(newCard, newCardImage);
-        return newCard;
-    }
+  constructor(cardData, placeTemplate, imagePopup) {
+    this._placeTemplate = placeTemplate;
+    this._cardData = cardData;
+    this._imagePopup = imagePopup;
+  }
 
-    _setAttributes(newCard, newCardImage) {
-        newCardImage.src = this._cardData.link;
-        newCard.querySelector("#titlePlace").textContent = this._cardData.name;
-        newCardImage.alt = this._cardData.name;
-    }
+  getElement() {
+    const newCard = this._placeTemplate.firstElementChild.cloneNode(true);
+    const newCardImage = newCard.querySelector(".place_url");
+    this._setEventListeners(newCard, newCardImage);
+    this._setAttributes(newCard, newCardImage);
+    return newCard;
+  }
 
-    _setEventListeners(newCard, newCardImage) {
-        const buttonLike = newCard.querySelector("#button-like");
-        buttonLike.addEventListener("click", () => {
-            buttonLike.classList.toggle("place__icon-like_active");
-          });
-        
-          newCard.querySelector("#button-trash").addEventListener("click", () => {
-            newCard.remove();
-          });
-        
-          newCardImage.addEventListener("click", () => {
-            this._imagePopup.open(this._cardData);
-          });
-    }
+  _setAttributes(newCard, newCardImage) {
+    newCardImage.src = this._cardData.link;
+    newCard.querySelector(".place__title").textContent = this._cardData.name;
+    newCardImage.alt = this._cardData.name;
+  }
+
+  _setEventListeners(newCard, newCardImage) {
+    this._addButtonLike(newCard);
+    this._removeCard(newCard); 
+    newCardImage.addEventListener("click", () => {
+      this._imagePopup.open(this._cardData);
+    });
+  }
+
+  _addButtonLike(newCard) {
+    const buttonLike = newCard.querySelector(".place__icon-like");
+    buttonLike.addEventListener("click", () => {
+      buttonLike.classList.toggle("place__icon-like_active");
+    });
+  }
+
+  _removeCard(newCard) {
+    newCard.querySelector(".place__icon-trash").addEventListener("click", () => {
+        newCard.remove();
+      });
+  }
 }
