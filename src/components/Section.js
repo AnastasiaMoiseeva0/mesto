aed90088c3de3860f7067ff515bcd15fbe956173
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-04-19 14:37:17 +0300

    hotfix: вместо card параметр фунции заменен на item

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,20 +5,9 @@ export default class Section {
     this._container = container;
   }
 
-  // getById(id) {
-  //   return this._items.find(item => item.getId() === id);
-  // }
-
-  addItem(card) {
-    this._container.prepend(card);
+  addItem(item) {
+    this._container.prepend(item);
   }
-
-  // removeItem(id) {
-  //   const indexToRemove = this._items.findIndex(card => card.getId() === id);
-  //   let [ card ] = this._items.splice(indexToRemove, 1);
-
-  //   card.removeCard();
-  // }
 
   renderItems(items) {
     items.forEach((item) => {
